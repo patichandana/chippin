d262c586cbd78e26737b35cf0b39cc63e33facc8
patichandana <iampati.official@gmail.com>
2025-08-02 16:22:40 +0530

    changed table names, Prisma model names to adhere to naming conventions

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,28 +1,22 @@
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 
-// console.log(prisma);
-
-async function main() {
-    await prisma.grouptypes.createMany({
-        data: [
-            {
-                "group_type_name": "Home"
-            },
-            {
-                "group_type_name": "Trip"
-            },
-            {
-                "group_type_name": "Friends"
-            },
-            {
-                "group_type_name": "Other"
-            }
-        ]
-    })
-}
-
-main().then(async () => {
+await prisma.groupTypes.createMany({
+    data: [
+        {
+            "group_type_name": "Home"
+        },
+        {
+            "group_type_name": "Trip"
+        },
+        {
+            "group_type_name": "Friends"
+        },
+        {
+            "group_type_name": "Other"
+        }
+    ]
+}).then(async () => {
     await prisma.$disconnect()
 }).catch(async (e) => {
     console.log(e);
