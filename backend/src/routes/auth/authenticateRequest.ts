e137c069ad630a5090dc90a3b9fe558158ed1ca8
patichandana <iampati.official@gmail.com>
2025-08-18 16:54:41 +0530

    added invalid jwt error message code

--- backend/src/routes/auth/authenticateRequest.ts
+++ backend/src/routes/auth/authenticateRequest.ts
@@ -1,5 +1,6 @@
 import jwt from "jsonwebtoken";
 import { jwtTokenUserSchema } from "../../interfaces/schemaDeclarations.js";
+import { ErrorResponse, genericErrorMessages } from "../../interfaces/ErrorHandlers/genericErrorHandler.js";
 
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 
@@ -13,10 +14,10 @@ export const authenticateRequest = (req, res) => {
             const a = jwtTokenUserSchema.parse(jwt.verify(jwtToken, JWT_SECRET_KEY));
             return a.userId;
         } catch (err) {
-            throw new Error("INVALID_JWT");
+            throw new ErrorResponse("INVALID_JWT", 500, genericErrorMessages["INVALID_JWT"].message, genericErrorMessages["INVALID_JWT"].details);
         }
     } else {
-        throw new Error("NO_AUTH_HEADER");
+        throw new ErrorResponse("NO_AUTH_HEADER", 500, genericErrorMessages["NO_AUTH_HEADER"].message, genericErrorMessages["NO_AUTH_HEADER"].details);
         //     "name": "noAuthHeader",
         //     "message": "user not logged in",
         //     "suggestion": "please authenticate"
