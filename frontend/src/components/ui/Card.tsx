89713d6824e8337f0e19f4ef815337131e581a8b
patichandana <iampati.official@gmail.com>
2025-11-10 17:47:08 +0530

    feature: get list of currencies api added

--- frontend/src/components/ui/Card.tsx
+++ frontend/src/components/ui/Card.tsx
@@ -3,11 +3,13 @@ import React from "react";
 interface CardProps extends React.HtmlHTMLAttributes<HTMLDivElement> {
   className?: string;
   children: React.ReactNode;
+  title?: string;
 }
 export default function Card({ className, children, ...props }: CardProps) {
   return (
-    <div className={`shadow-md bg-white rounded-xl p-10 ${className}`} {...props}>
-      {children}
+    <div className={className}>
+      <p className="text-3xl font-light my-4 m-10 mb-0">{props.title}</p>
+      <div className={`shadow-md bg-white rounded-xl p-10 m-10 mt-5`}>{children}</div>
     </div>
   );
 }
