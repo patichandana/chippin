313c2136179edd802eb20b0683f499f89244f9d9
Chandra Lekha Chitta <chandu@Mac.lan>
2025-12-09 19:39:28 +0530

    added footer to landing page

---
+++ frontend/src/components/ui/Footer.tsx
@@ -0,0 +1,19 @@
+export default function Footer() {
+  return (
+    <footer className="w-full bg-gray-700 text-gray-300 py-6 mt-10">
+      <div className="max-w-6xl mx-auto px-6 flex flex-col md:flex-row justify-between items-center gap-4">
+        
+        {/* Left  */}
+        <p className="text-sm">
+          chippin @ {new Date().getFullYear()}. All rights reserved.
+        </p>
+
+        {/* Right links */}
+        <div className="flex gap-6 text-sm">
+          <a href="#" className="hover:text-white transition">About</a>
+          <a href="#" className="hover:text-white transition">Contact</a>
+        </div>
+      </div>
+    </footer>
+  );
+}
